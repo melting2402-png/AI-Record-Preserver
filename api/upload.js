dbeec3c11573c061a00d8c329f5d01416d7881b0
LilRoGoPro1 <melting2402@gmail.com>
2026-07-01 21:24:12 +0530

    Update upload.js

--- api/upload.js
+++ api/upload.js
@@ -1,1 +1,31 @@
+import { GoogleGenAI } from "@google/genai";
 
+export default async function handler(req, res) {
+
+    if (req.method !== "POST") {
+        return res.status(405).json({
+            error: "Only POST allowed"
+        });
+    }
+
+    try {
+
+        const ai = new GoogleGenAI({
+            apiKey: process.env.GEMINI_API_KEY
+        });
+
+        const uploadUrl = await ai.files.createUploadUrl();
+
+        res.status(200).json(uploadUrl);
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+            error: err.message
+        });
+
+    }
+
+}
